160b79408a84b1992a78197a98592612268f2ff8
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2023-04-22 16:46:54 -0600

    Adding API/character

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import Navbar from './components/Navbar'
 
+fetch("https://rickandmortyapi.com/api/character")
+  .then(response => response.json())
+  .then(data =>console.log(data))
+  .catch(error => console.log(error))
+
 function App() {
   return (
     <Navbar brand="Rick and Morty App" />

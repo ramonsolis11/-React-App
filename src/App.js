e3a4380dc1400e7d74648ba1ac1631994f794a04
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2023-04-22 23:28:25 -0600

    components
    Characters.js
    Navbar.js
    Pagination.js
    App.js
    index.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
-import Navbar from './components/Navbar'
-
-fetch("https://rickandmortyapi.com/api/character")
-  .then(response => response.json())
-  .then(data =>console.log(data))
-  .catch(error => console.log(error))
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import Characters from "./components/Characters";
+import Pagination from "./components/Pagination";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+  const [info, setInfo] = useState({});
+
+  const initialUrl = "https://rickandmortyapi.com/api/character";
+
+  const fetchCharacters = (url) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCharacters(data.results);
+        setInfo(data.info);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const onPrevious = () => {
+    fetchCharacters(info.prev);
+  };
+
+  const onNext = () => {
+    fetchCharacters(info.next);
+  };
+
+  useEffect(() => {
+    fetchCharacters(initialUrl);
+  }, []);
+
   return (
-    <Navbar brand="Rick and Morty App" />
+    <>
+      <Navbar brand="Rick and Morty App" />
+
+      <div className="container mt-5">
+        <Pagination
+          prev={info.prev}
+          next={info.next}
+          onPrevious={onPrevious}
+          onNext={onNext}
+        />
+        <Characters characters={characters} />
+        <Pagination
+          prev={info.prev}
+          next={info.next}
+          onPrevious={onPrevious}
+          onNext={onNext}
+        />
+      </div>
+    </>
   );
 }
 
